39408ba623e39d2591313015271f65cb3e4e0934
Michael-amali <michael.acheampong@amalitech.com>
2021-12-15 12:47:25 +0100

    Add admin dashboard routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Admin from "../views/Admin.vue";
+import Overview from "../views/Overview.vue";
+import Products from "../views/Products.vue";
+import Orders from "../views/Orders.vue";
 
 Vue.use(VueRouter);
 
@@ -13,11 +17,27 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+  },
+  {
+    path: "/admin/overview",
+    name: "Overview",
+    component: Overview,
+  },
+  {
+    path: "/admin/products",
+    name: "Products",
+    component: Products,
+  },
+  {
+    path: "/admin/orders",
+    name: "Orders",
+    component: Orders,
   },
 ];
 
