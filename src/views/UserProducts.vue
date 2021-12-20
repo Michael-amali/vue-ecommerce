188a888bf46fabf64cd0a2b7704cc1ab69622bc7
Michael-amali <michael.acheampong@amalitech.com>
2021-12-20 10:42:14 +0100

    Add Products page and Footer component

---
+++ src/views/UserProducts.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid class="my-0 mx-auto pa-0">
+    <Navbar />
+    <section>
+      <v-container class="mt-16">
+        <v-row class="pa-4">
+          <v-col class="col-md-6 my-auto">
+            <p class="text-center text-h4">IN PROGRESS</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores assumenda molestiae ipsum. Omnis, voluptatum odit.
+              Harum aspernatur ad, voluptatibus earum consectetur veniam
+              deleniti aliquam veritatis rem quod itaque vero obcaecati!
+              Reiciendis voluptates, ipsam quas hic
+            </p>
+          </v-col>
+          <v-col class="col-md-6">
+            <v-img src="../assets/svg/products_cart.svg"></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+    <MiniCart />
+  </v-container>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import MiniCart from "@/components/MiniCart.vue";
+export default {
+  name: "UserProducts",
+  components: { Navbar, MiniCart },
+
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
